fc8fc74d48e4bcb609602c846f248c3e3eba42c9
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-10-08 18:28:51 -0400

    Set autocrlf to true, and reset hard to HEAD. Hopefully this cleans out all CRLFs.

--- watir/lib/watir/locator.rb
+++ watir/lib/watir/locator.rb
@@ -34,6 +34,8 @@ module Watir
     end    
 
     def locate
+      index_target = @specifiers[:index]
+
       count = 0
       each_element(@tag) do |element|
         
@@ -45,7 +47,7 @@ module Watir
             end
           end
           count += 1
-          unless count == @specifiers[:index]
+          unless index_target == count
             throw :next_element
           end
           return element.ole_object          
@@ -83,56 +85,43 @@ module Watir
     end
     def specifier= arg
       how, what, value = arg
-
-      if how.class == Hash and what.nil?
-        specifiers = how
-      else
-        specifiers = {how => what}
-      end
-
-      @specifiers = {:index => 1} # default if not specified
-      @specifiers[:value] = value.to_s if value
-      specifiers.each do | how, what |
-        how = :value if how == :caption
-        how = :class_name if how == :class
-        what = what.to_i if how == :index
-        @specifiers[how] = what
-      end
+      how = :value if how == :caption
+      how = :class_name if how == :class
+      what = what.to_i if how == :index
+      value = value.to_s if value 
+      @how = how
+      @what = what
+      @value = value
     end
-
     def locate
-      count = 0
+      object_index = 1
       @elements.each do |object|
         element = Element.new(object)
-
-        catch :next_element do
-          throw :next_element unless @types.include?(element.type)
-          @specifiers.each do |how, what|
-            next if how == :index
-            unless match? element, how, what
-              throw :next_element
+        if @types.include?(element.type)
+          if @how == :index
+            attribute = object_index
+          else
+            begin
+              attribute = element.send(@how)
+            rescue NoMethodError
+              raise MissingWayOfFindingObjectException,
+                "#{@how} is an unknown way of finding an <INPUT> element (#{@what})"
             end
           end
-          count += 1
-          throw :next_element unless count == @specifiers[:index]
-          return object
+          if @what.matches(attribute)
+            if @value
+              if element.value == @value
+                return object
+              end
+            else
+              return object
+            end
+          end
+          object_index += 1
         end
-
       end
-      nil
+      return nil
     end
-    # return true if the element matches the provided how and what
-    def match? element, how, what
-      begin
-        attribute = element.send(how)
-      rescue NoMethodError
-        raise MissingWayOfFindingObjectException,
-          "#{how} is an unknown way of finding an <INPUT> element (#{what})"
-      end
-
-      what.matches(attribute)
-    end
-    
     def fast_locate
       # Searching through all elements returned by ole_inner_elements
       # is *significantly* slower than IE's getElementById() and
@@ -142,20 +131,20 @@ module Watir
       # where the :name matches, so we will only return the results of
       # getElementById() if the matching element actually HAS a matching
       # :id.
-
-      the_id = @specifiers[:id]
-      if the_id && the_id.class == String && 
-        @specifiers[:index] == 1 && @specifiers.length == 2
-        @element = @document.getElementById(the_id) rescue nil
-        # Return if our fast match really HAS a matching :id
-        return true if @element && @element.invoke('id') == the_id
-      end
-
-      the_name = @specifiers[:name]
-      if the_name && the_name.class == String
-        @elements = @document.getElementsByName(the_name) rescue nil
-      end
-      false
+      return unless @what.class == String # Only use fast calls with String what.
+      begin
+        case @how
+          when :id
+            @element = @document.getElementById(@what)
+            # Return if our fast match really HAS a matching :id
+            return true if @element && @element.invoke('id') == what
+          when :name
+            @elements = @document.getElementsByName(@what)
+        end
+        false
+      rescue
+        false
+      end      
     end
   end
 end    
